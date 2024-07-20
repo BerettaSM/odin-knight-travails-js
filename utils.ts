74751f15760834bd99a0b33ae0cabeea7812e732
Ramon Saviato <ramonsaviato@hotmail.com>
2024-07-19 23:15:13 -0300

    add range helper function

--- utils.ts
+++ utils.ts
@@ -7,3 +7,38 @@ export function randomNumber(min: number, max: number) {
 export function getRandomPosition(): Position {
     return [randomNumber(0, 7), randomNumber(0, 7)];
 }
+
+export function range(endExclusive: number): number[];
+export function range(startInclusive: number, endExclusive: number): number[];
+export function range(
+    startInclusive: number,
+    endExclusive: number,
+    step: number
+): number[];
+export function range(
+    startInclusive: number,
+    endExclusive?: number,
+    step: number = 1
+): number[] {
+    if (endExclusive === undefined) {
+        endExclusive = startInclusive;
+        startInclusive = 0;
+    }
+    const result: number[] = [];
+    const isIncrementing = startInclusive < endExclusive;
+    if (step === 0) {
+        console.warn('You passed in zero as step value. Defaulting to 1.');
+        step = 1;
+    }
+    if ((isIncrementing && step < 0) || (!isIncrementing && step > 0)) {
+        step *= -1;
+    }
+    for (
+        let i = startInclusive;
+        isIncrementing ? i < endExclusive : i > endExclusive;
+        i += step
+    ) {
+        result.push(i);
+    }
+    return result;
+}
