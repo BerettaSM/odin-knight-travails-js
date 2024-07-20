9da6842f685e51b7f573743300bb6efcbaa4b128
Ramon Saviato <ramonsaviato@hotmail.com>
2024-07-19 22:24:19 -0300

    rearrange: move piece constructor type to type.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('Hey there');
+import type { PieceConstructor } from './types';
+import { Knight, pieceMoves } from './chess';
+
+const pieces: Record<string, PieceConstructor> = {
+    Knight,
+};
+
+for (const [name, ChessPiece] of Object.entries(pieces)) {
+    const piece = new ChessPiece({ position: [0, 0] });
+
+    console.log(name, pieceMoves(piece, [5, 5]));
+}
--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
-import { Piece } from "./chess";
+import { Piece } from './chess';
 
 export type Position = [number, number];
 
@@ -6,3 +6,7 @@ export type PieceConfig = {
     lastPosition?: Piece;
     position?: Position;
 };
+
+export type PieceConstructor = new (
+    ...args: ConstructorParameters<typeof Piece>
+) => Piece;
